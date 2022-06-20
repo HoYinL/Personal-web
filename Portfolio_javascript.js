e73b8733706a7c4ee6bcf6ed750374c3f955cad1
HoYinL <107821162+HoYinL@users.noreply.github.com>
2022-06-20 13:50:35 +0800

    Update Portfolio_javascript.js

--- Portfolio_javascript.js
+++ Portfolio_javascript.js
@@ -58,11 +58,10 @@ window.addEventListener("scroll", () => {
 //---------------------- workplace list and workplace content -------------------//
 let main = document.getElementById("content");
 
-window.addEventListener("load", ()=>{
-    let workplace = document.getElementById("workplace");
-    let workplace_li = workplace.querySelectorAll(`li`);
-    
+window.addEventListener("load", ()=>{    
 $(document).ready(function() {
+let workplace = document.getElementById("workplace");
+let workplace_li = workplace.querySelectorAll(`li`);
 let experience_sec = document.getElementById("experience")
 let includedContent = document.getElementById(`includedContent`);
 let workplace_light = document.getElementById(`workplace-light`);
