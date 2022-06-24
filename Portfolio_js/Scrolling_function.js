6292814697d5f340ddaa7a5fd2fc6c59a1343578
HoYinL <107821162+HoYinL@users.noreply.github.com>
2022-06-24 18:29:52 +0800

    Update Scrolling_function.js
    Signed-off-by: HoYinL <107821162+HoYinL@users.noreply.github.com>

--- Portfolio_js/Scrolling_function.js
+++ Portfolio_js/Scrolling_function.js
@@ -60,6 +60,7 @@ $(document).ready(function() {
     let Project_files_container2 =  document.getElementById(`Project-files-container`);
     let Project_files_parents2 = Project_files_container2.children;
     if(Project_files_container2 == null){
+        console.log(`jj`)
         reload();
     }
     
