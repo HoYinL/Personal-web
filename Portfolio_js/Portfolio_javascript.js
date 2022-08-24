424fd02286c8ddd966298cb1096d1b8f4000684d
HoYinL <107821162+HoYinL@users.noreply.github.com>
2022-08-24 21:22:06 +0800

    Update Portfolio_javascript.js
    Signed-off-by: HoYinL <107821162+HoYinL@users.noreply.github.com>

--- Portfolio_js/Portfolio_javascript.js
+++ Portfolio_js/Portfolio_javascript.js
@@ -208,26 +208,6 @@ $(document).ready(function() {
 })
 })
 
-let Project_files_cover1 = document.getElementsByClassName("Project-files-Parents");
-let Project_container1 = document.getElementById("Project-files-container"); 
-let Project_files_h41 = Project_container.getElementsByTagName("h4");
-let Show_hidden1 = document.getElementById("show-hidden");
-       
-        // Sets the general interface of the section
-        // Shows first 6 files and hidden all the following files (by default)
-        for(let ele of Project_files_cover1){
-            let currentFileIndex = Array.from(Project_files_cover1).indexOf(ele);
-
-            if(currentFileIndex > 5){
-                Project_files_cover1[currentFileIndex].classList.add("Project-files-absolute");
-                setTimeout(() => {  
-                    //Project_files_cover1[currentFileIndex].classList.replace("Project-files-displayNone", "Project-files-absolute");
-                    Project_files_cover1[currentFileIndex].classList.add("Project-files-opacity"); 
-                    Project_files_cover1[currentFileIndex].classList.add("Project-files-transform"); 
-                }, 0) 
-            }
-        }
-
 //-conatiner (Section 2)
 window.addEventListener("load", ()=>{
 $(document).ready(function() {
@@ -242,9 +222,8 @@ $(document).ready(function() {
         for(let ele of Project_files_cover){
             let currentFileIndex = Array.from(Project_files_cover).indexOf(ele);
 
-            /*
             if(currentFileIndex > 5){
-                Project_files_cover[currentFileIndex].classList.add("Project-files-absolute");
+                Project_files_cover[currentFileIndex].classList.add("Project-files-displayNone");
                 setTimeout(() => {  
                     //Project_files_cover[currentFileIndex].classList.replace("Project-files-displayNone", "Project-files-absolute");
                     Project_files_cover[currentFileIndex].classList.add("Project-files-opacity"); 
@@ -252,6 +231,7 @@ $(document).ready(function() {
                 }, 0) 
             }
             
+            /*
             if(currentFileIndex > 5){
                 //Project_files_cover[currentFileIndex].classList.add("Project-files-absolute");
                 //Project_files_cover[currentFileIndex].classList.add("Project-files-displayNone");
