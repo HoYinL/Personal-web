2284a91ba2e623f7cc86e165335dc106c569946f
HoYinL <107821162+HoYinL@users.noreply.github.com>
2022-08-24 23:08:50 +0800

    Update Portfolio_javascript.js
    Signed-off-by: HoYinL <107821162+HoYinL@users.noreply.github.com>

--- Portfolio_js/Portfolio_javascript.js
+++ Portfolio_js/Portfolio_javascript.js
@@ -328,7 +328,7 @@ document.body.addEventListener("pointerdown", (event)=>{
 // ------------------- Responsive nav-list ------------------------------------- //
 
 //------------------------------------------------------------------------------ //
-window.addEventListener("load", ()=>{
+window.addEventListener("onload", ()=>{
     setTimeout(()=>{
         var img = document.createElement('img'); 
         img.src = 'https://images.unsplash.com/photo-1453728013993-6d66e9c9123a?ixid=MnwxMjA3fDB8MHxzZWFyY2h8Mnx8dmlld3xlbnwwfHwwfHw%3D&ixlib=rb-1.2.1&w=1000&q=80'; 
